9c48f023bc5bd24c5298cc6c0f3490087f01a9b8
HamzaBee <hamzabenbrahim6060@gmail.com>
2026-04-19 16:19:00 +0100

    Creating product repository

--- src/main/java/ma/enset/universitytp2/repository/ProductRepo.java
+++ src/main/java/ma/enset/universitytp2/repository/ProductRepo.java
@@ -2,7 +2,7 @@ package ma.enset.universitytp2.repository;
 
 import ma.enset.universitytp2.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
+
 
 public interface ProductRepo extends JpaRepository<Product, Long> {
 }
