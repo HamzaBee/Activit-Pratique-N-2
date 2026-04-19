cfcca35fa6195352990d328fee461e1e6a8afc9f
HamzaBee <hamzabenbrahim6060@gmail.com>
2026-04-19 16:14:21 +0100

    Entity product

---
+++ src/main/java/ma/enset/universitytp2/UniversityTp2Application.java
@@ -0,0 +1,13 @@
+package ma.enset.universitytp2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversityTp2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UniversityTp2Application.class, args);
+    }
+
+}
---
+++ src/test/java/ma/enset/universitytp2/UniversityTp2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.enset.universitytp2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversityTp2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
