9d35397f218a0bb1289b67c123cc71f9dbfa082b
HamzaBee <hamzabenbrahim6060@gmail.com>
2026-04-19 17:32:30 +0100

    adding delete and saving product operations

--- src/main/java/ma/enset/universitytp2/controller/ProductController.java
+++ src/main/java/ma/enset/universitytp2/controller/ProductController.java
@@ -1,17 +1,15 @@
 package ma.enset.universitytp2.controller;
 
-import lombok.RequiredArgsConstructor;
+
 import ma.enset.universitytp2.entity.Product;
 import ma.enset.universitytp2.repository.ProductRepo;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-
+@RequestMapping("/api")
 public class ProductController {
     @Autowired
     private ProductRepo productRepo;
@@ -23,7 +21,19 @@ public class ProductController {
 
     @GetMapping("/products/{id}")
     public Product findById(@PathVariable Long id){
-        return productRepo.findById(id).get();
+        return productRepo.findById(id).orElseThrow(()->new RuntimeException("product not found"));
+    }
+    @GetMapping("/find/products")
+    public  List<Product> findProductByName(@RequestParam(name = "kw", defaultValue = "") String kw){
+        return productRepo.findByNameContains(kw);
+    }
+    @PostMapping("/products")
+    public Product save(@RequestBody Product product){
+        return productRepo.save(product);
+    }
+    @DeleteMapping("/products/{id}")
+    public void deleteById(@PathVariable Long id){
+        productRepo.deleteById(id);
     }
 
 
--- src/main/java/ma/enset/universitytp2/repository/ProductRepo.java
+++ src/main/java/ma/enset/universitytp2/repository/ProductRepo.java
@@ -3,7 +3,9 @@ package ma.enset.universitytp2.repository;
 import ma.enset.universitytp2.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 
 public interface ProductRepo extends JpaRepository<Product, Long> {
-
+     List<Product> findByNameContains(String kw);
 }
